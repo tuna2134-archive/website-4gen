2c12b287c195b2fbe03675aec48979df62a209a7
コマリン親衛隊 <contact@tuna2134.dev>
2024-03-12 23:42:43 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Avatar from "@/components/avatar";
+import * as BaseAvatar from "@/components/avatar";
 import fetcher, { API_ENDPOINT } from "@/libs/fetch";
 import Link from "next/link";
 import useSWR from "swr";
@@ -15,17 +15,7 @@ interface ContentProps {
   children: React.ReactNode;
 }
 
-const Content: React.FC<ContentProps> = ({ children }) => {
-  return (
-    <main className="h-screen">
-      <div className="flex h-full flex-col items-center justify-center space-y-2">
-        {children}
-      </div>
-    </main>
-  );
-};
-
-export default function Home() {
+const Avatar: React.FC<ContentProps> = () => {
   const { data, error, isLoading } = useSWR<UserStatus>(
     `${API_ENDPOINT}/status`,
     fetcher,
@@ -38,18 +28,23 @@ export default function Home() {
     );
   if (error)
     return (
-      <Content>
-        <p>{`Error: ${error}`}</p>
-      </Content>
+      <p>{`Error: ${error}`}</p>
     );
   return (
-    <Content>
-      <Avatar status={data?.status} src={AVATAR_URL} />
+    <Avatar status={data?.status} src={AVATAR_URL} />
+  );
+};
+
+export default function Home() {
+  return (
+    <main className="h-screen">
+      <div className="flex h-full flex-col items-center justify-center space-y-2">
+      <Avatar />
       <h1 className="text-center text-3xl font-bold">tuna2134</h1>
       <span>アニメなどの趣味に没頭する豚です</span>
       <Link href="/home" className="rounded bg-lime-700 px-6 py-2 text-white">
         Go!!!
       </Link>
-    </Content>
+    </div></main>
   );
 }
