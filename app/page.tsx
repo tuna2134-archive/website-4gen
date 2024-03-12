be16d6cc4494b33f088e549d2a4fd836c5345229
コマリン親衛隊 <contact@tuna2134.dev>
2024-03-12 23:35:04 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,6 @@ const Content: React.FC<ContentProps> = () => {
   return (
     <main className="h-screen">
       <div className="flex h-full flex-col items-center justify-center space-y-2">
-        <Avatar status={data?.status} src={AVATAR_URL} />
-        <h1 className="text-center text-3xl font-bold">tuna2134</h1>
-        <span>アニメなどの趣味に没頭する豚です</span>
-        <Link href="/home" className="rounded bg-lime-700 px-6 py-2 text-white">
-          Go!!!
-        </Link>
       </div>
     </main>
   )
