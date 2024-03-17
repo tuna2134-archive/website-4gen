c8e5ef25ad17b1abf84a674ba5485f6b2ab9546b
tuna2134 <contact@tuna2134.dev>
2024-03-17 14:09:51 +0900

    Add iconURL prop to Card component and display icon if provided

---
+++ app/(pages)/contact/page.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import Image from "next/image";
+import config from "@/config.json";
+import Card from "@/components/card";
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <h2 className="mb-4 text-2xl font-bold">Contact</h2>
+      <div className="space-y-4">
+        {config.pages.contacts.map((contact, index) => (
+          <Card title={contact.name} iconURL={contact.image} key={index}>
+            <a
+              href={contact.href}
+              className="rounded px-2 py-1 hover:bg-lime-100 hover:text-lime-700"
+            >
+              {contact.hrefTitle}
+            </a>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Page;
--- components/card.tsx
+++ components/card.tsx
@@ -1,15 +1,22 @@
 import clsx from "clsx";
+import Image from "next/image";
 
 interface CardProps {
   title: string;
   children: React.ReactNode;
   className?: string;
+  iconURL?: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, children, className }) => {
+const Card: React.FC<CardProps> = ({ title, children, className, iconURL }) => {
   return (
     <div className={clsx("rounded-md bg-white p-4 shadow-md", className)}>
-      <h3 className="text-2xl font-bold">{title}</h3>
+      <div className="flex space-x-2 items-center">
+        {iconURL && (
+          <Image src={iconURL as string} alt={title} width={32} height={32} />
+        )}
+        <h3 className="text-2xl font-bold">{title}</h3>
+      </div>
       <div className="mt-4">{children}</div>
     </div>
   );
