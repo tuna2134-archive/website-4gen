85a96431cfea613908a8fa3a813bb21b9e920cdb
tuna2134 <contact@tuna2134.dev>
2024-03-17 14:19:19 +0900

    fin

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import { MetadataRoute } from "next";
+
+const sitemaps = [
+  {
+    path: "/",
+    priority: 1,
+  },
+  {
+    path: "/home",
+    priority: 0.9,
+  },
+  {
+    path: "/articles",
+    priority: 0.8,
+  },
+  {
+    path: "/works",
+    priority: 0.8,
+  },
+  {
+    path: "/contact",
+    priority: 0.3,
+  }
+]
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return sitemaps.map((sitemap) => ({
+    url: `https://tuna2134.jp${sitemap.path}`,
+    changeFrequency: "daily",
+    lastModified: new Date(),
+    priority: sitemap.priority,
+  }));
+};
