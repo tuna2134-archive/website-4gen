52dbf3d0f8142c34245c7b3def277175ddcab7bf
コマリン親衛隊 <contact@tuna2134.dev>
2024-03-27 19:40:21 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
-        "fade-in-right":
-          "fade-in-right 0.3s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+        "fade-in-top": "fade-in-top 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both"
       },
       keyframes: {
-        "fade-in-right": {
-          "0%": {
-            transform: "translateX(50px)",
-            opacity: "0",
-          },
-          to: {
-            transform: "translateX(0)",
-            opacity: "1",
-          },
-        },
-      },
+                "fade-in-top": {
+                    "0%": {
+                        transform: "translateY(-50px)",
+                        opacity: "0"
+                    },
+                    to: {
+                        transform: "translateY(0)",
+                        opacity: "1"
+                    }
+                }
+            }
     },
   },
   plugins: [],
