e7ce3a8f793f9eab526e4bb25e71f614f4834dca
--global <--global>
2024-04-05 22:04:34 +0900

    スマホの対応

--- app/(not-pages)/services/page.tsx
+++ app/(not-pages)/services/page.tsx
@@ -19,7 +19,7 @@ const Page: NextPage = () => {
     <div className="h-screen w-full flex items-center justify-center">
       <div>
         <h1 className="text-3xl font-bold text-center">サービス一覧</h1>
-        <div className="pt-6 grid gap-2 grid-cols-2">
+        <div className="pt-6 grid gap-2 grid-cols-1 md:grid-cols-2">
           {services.map((card, index) => (
             <Card title={card.name} key={index} href={card.href}>
               {card.description}
