3175e1732c659e87af173a924bb074c421a7e182
コマリン親衛隊 <contact@tuna2134.dev>
2024-05-25 21:19:07 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import useSWR from "swr";
 
 const AVATAR_URL: string =
-    "https://cdn.discordapp.com/avatars/739702692393517076/b9fddaa026c1002e8c7d4d8f22978e43.png?size=1024";
+    "https://cdn.discordapp.com/avatars/739702692393517076/689da5236709ab59a0371b7abcac7bda.png?size=1024";
 
 interface UserStatus {
     status: "online" | "offline" | "idle" | undefined;
